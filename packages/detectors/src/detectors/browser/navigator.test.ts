6e7c101c3b277d9546cfb16e7f5edd0c85eb6dba
Benjamin Strasser <benjamin.strasser@aots.io>
2026-02-11 10:54:27 +0100

    ci: update node version to 24
    Signed-off-by: Benjamin Strasser <benjamin.strasser@aots.io>

--- packages/detectors/src/detectors/browser/navigator.test.ts
+++ packages/detectors/src/detectors/browser/navigator.test.ts
@@ -4,13 +4,26 @@ import type { Locale } from '../../../../runtime/src/core.mjs'
 import { navigatorDetector } from './navigator.mjs'
 
 const test = suite('detector:navigator')
+const originalNavigatorDescriptor = Object.getOwnPropertyDescriptor(globalThis, 'navigator')
 
 // --------------------------------------------------------------------------------------------------------------------
 
 const testDetector = (name: string, languages: string[] | undefined, expected: Locale[]) =>
 	test(`navigator ${name}`, () => {
-		globalThis.navigator = { languages: languages as string[] } as unknown as Navigator
-		assert.equal(navigatorDetector(), expected)
+		Object.defineProperty(globalThis, 'navigator', {
+			configurable: true,
+			value: { languages: languages as string[] } as unknown as Navigator,
+		})
+
+		try {
+			assert.equal(navigatorDetector(), expected)
+		} finally {
+			if (originalNavigatorDescriptor) {
+				Object.defineProperty(globalThis, 'navigator', originalNavigatorDescriptor)
+			} else {
+				delete (globalThis as { navigator?: Navigator }).navigator
+			}
+		}
 	})
 
 testDetector('undefined', undefined, [])
